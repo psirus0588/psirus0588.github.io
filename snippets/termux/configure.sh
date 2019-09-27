da5a745a65a5c9052822d08e0d56c7c28fd53f61
psirus0588 <psirus0588@gmail.com>
2019-09-27 12:35:00 +0300

    Update configure.sh

--- snippets/termux/configure.sh
+++ snippets/termux/configure.sh
@@ -1,5 +1,11 @@
 # Install needed packages:
-pkg install -y wget htop git
+pkg install -y wget proot
+ htop git php, python
+
+# Install composer
+curl -L https://github.com/yuloh/composer-termux/releases/download/1.6.2/composer.phar -o composer.phar
+chmod +x ./composer.phar
+mv ./composer.phar /data/data/com.termux/files/usr/bin/composer
 
 touch ../usr/etc/resolv.conf
 echo 'nameserver 8.8.8.8' > ../usr/etc/resolv.conf
