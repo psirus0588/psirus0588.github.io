5f507e3ca7067b4834ea1fbbc9dd5bd30307a78b
psirus0588 <psirus0588@gmail.com>
2019-09-30 13:00:49 +0300

    Update merge_two_repositories_into_one.sh

--- snippets/git/merge_two_repositories_into_one.sh
+++ snippets/git/merge_two_repositories_into_one.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-# Use merge_two_repositories_into_one.sh [repository_one_name] [repository_one_url] [repository_two_name] [repository_two_url]
+# Use merge_two_repositories_into_one.sh [new_repository_name] [repository_one_name] [repository_one_url] [repository_two_name] [repository_two_url]
 # ToDo add loop to merge many repositories from list in file .git_merged_repositories
 
+mkdir $1 && cd $1
 git init
 
-echo "add repository $1" > merge_two_repositories.txt
+echo "add repository $2" > merge_two_repositories.txt
 git add .
-git commit -m "add repository $1"
-git remote add -f $1 $2
-git merge $1/master
-mkdir $1
-dir –exclude $1 | %{git mv $_.Name $1}
-git commit -m “Move $1 files into subdir”
+git commit -m "add repository $2"
+git remote add -f $2 $3
+git merge $2/master
+mkdir $2
+dir –exclude $2 | %{git mv $_.Name $2}
+git commit -m “Move $2 files into subdir”
 
-git remote add -f $3 $4
-git merge $3/master
-mkdir $3
-dir –exclude $3 | %{git mv $_.Name $3}
-git commit -m “Move $1 files into subdir”
+git remote add -f $4 $5
+git merge $4/master
+mkdir $4
+dir –exclude $4 | %{git mv $_.Name $4}
+git commit -m “Move $4 files into subdir”
