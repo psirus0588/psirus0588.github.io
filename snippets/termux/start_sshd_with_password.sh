5e921207bb69570b24e0e3ffdce37b7ce1bb46d5
psirus0588 <psirus0588@gmail.com>
2019-09-30 14:26:23 +0300

    Update start_sshd_with_password.sh

--- snippets/termux/start_sshd_with_password.sh
+++ snippets/termux/start_sshd_with_password.sh
@@ -3,4 +3,4 @@ passwd
 pkg install -y ssh
 sshd
 IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-echo "For connect to device from desktop use command: ssh $USER@IP -p 8022
+echo "For connect to device from desktop use command: ssh $USER@IP -p 8022"
