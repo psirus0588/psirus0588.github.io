49412244a8ef3c66c2af73e2e4f940d3fded4d38
psirus0588 <psirus0588@gmail.com>
2019-10-02 16:15:55 +0300

    Update gcloud_advance.sh

--- snippets/gcloud/gcloud_advance.sh
+++ snippets/gcloud/gcloud_advance.sh
@@ -16,22 +16,25 @@ if [ $1 = "create" ]; then
   MANAGE_ZONE=$3
   DOMAIN=$4
   IP=$5
+  
+  #Create project
+  gcloud alpha projects create $PROJECT --name="$PROJECT"
+
+  #Create dns zone
+  gcloud beta dns --project=$PROJECT managed-zones create $MANAGE_ZONE --description= --dns-name=$DOMAIN.
+  #OUTPUT: Created [https://dns.googleapis.com/dns/v1beta2/projects/$PROJECT/managedZones/$MANAGE_ZONE].
+
+  # ADD RECORDS
+  gcloud dns --project=$PROJECT record-sets transaction start --zone=$MANAGE_ZONE
+  gcloud dns --project=$PROJECT record-sets transaction add $IP --name=\*.$DOMAIN. --ttl=$TTL --type=A --zone=$MANAGE_ZONE
+  gcloud dns --project=$PROJECT record-sets transaction add $IP --name=@.$DOMAIN. --ttl=$TTL --type=A --zone=$MANAGE_ZONE
+  gcloud dns --project=$PROJECT record-sets transaction execute --zone=$MANAGE_ZONE
+
 else
   echo "command not exist"
   exit 1
 fi
 
-#Create project
-gcloud alpha projects create $PROJECT --name="$PROJECT"
 
-#Create dns zone
-gcloud beta dns --project=$PROJECT managed-zones create $MANAGE_ZONE --description= --dns-name=$DOMAIN.
-#OUTPUT: Created [https://dns.googleapis.com/dns/v1beta2/projects/$PROJECT/managedZones/$MANAGE_ZONE].
-
-# ADD RECORDS
-gcloud dns --project=$PROJECT record-sets transaction start --zone=$MANAGE_ZONE
-gcloud dns --project=$PROJECT record-sets transaction add $IP --name=\*.$DOMAIN. --ttl=$TTL --type=A --zone=$MANAGE_ZONE
-gcloud dns --project=$PROJECT record-sets transaction add $IP --name=@.$DOMAIN. --ttl=$TTL --type=A --zone=$MANAGE_ZONE
-gcloud dns --project=$PROJECT record-sets transaction execute --zone=$MANAGE_ZONE
 
 exit 0
