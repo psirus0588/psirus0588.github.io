b8366e6b6bd240bd7719c184746391b2858c1ed1
psirus0588 <psirus0588@gmail.com>
2019-10-02 15:57:06 +0300

    Update gcloud_advance.sh

--- snippets/gcloud/gcloud_advance.sh
+++ snippets/gcloud/gcloud_advance.sh
@@ -10,6 +10,9 @@ IP=$4
 TTL=604800
 #ttl 604800 for one week 
 
+#Create project
+gcloud alpha projects create $PROJECT --name="$PROJECT"
+
 #Create dns zone
 gcloud beta dns --project=$PROJECT managed-zones create $MANAGE_ZONE --description= --dns-name=$DOMAIN.
 #OUTPUT: Created [https://dns.googleapis.com/dns/v1beta2/projects/$PROJECT/managedZones/$MANAGE_ZONE].
