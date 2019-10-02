a160b6311580105ebae39516747b004c6d39407d
psirus0588 <psirus0588@gmail.com>
2019-10-02 15:23:20 +0300

    Create create_dns_and_ip_record.sh

---
+++ snippets/gcloud/create_dns_and_ip_record.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+#GCloud
+
+PROJECT=$1
+MANAGE_ZONE=$2
+DOMAIN=$3
+IP=$4
+TTL=604800
+#ttl 604800 for one week 
+
+#Create dns zone
+gcloud beta dns --project=$PROJECT managed-zones create $MANAGE_ZONE --description= --dns-name=$DOMAIN.
+#OUTPUT: Created [https://dns.googleapis.com/dns/v1beta2/projects/$PROJECT/managedZones/$MANAGE_ZONE].
+
+# ADD RECORDS
+gcloud dns --project=$PROJECT record-sets transaction start --zone=$MANAGE_ZONE
+gcloud dns --project=$PROJECT record-sets transaction add $IP --name=\*.$DOMAIN. --ttl=$TTL --type=A --zone=$MANAGE_ZONE
+gcloud dns --project=$PROJECT record-sets transaction add $IP --name=@.$DOMAIN. --ttl=$TTL --type=A --zone=$MANAGE_ZONE
+gcloud dns --project=$PROJECT record-sets transaction execute --zone=$MANAGE_ZONE
+
+exit 0
