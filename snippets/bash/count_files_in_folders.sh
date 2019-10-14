491fede0f169909e2339b47369ee0275f32d62e5
psirus0588 <psirus0588@gmail.com>
2019-10-14 14:52:04 +0300

    Update count_files_in_folders.sh

--- snippets/bash/count_files_in_folders.sh
+++ snippets/bash/count_files_in_folders.sh
@@ -2,3 +2,11 @@ for i in */ .*/ ; do
     echo -n $i": " ; 
     (find "$i" -type f | wc -l) ; 
 done
+
+
+#OR
+#find . -type d -print0 | while read -d '' -r dir; do
+#    files=("$dir"/*)
+#    printf "%5d files in directory %s\n" "${#files[@]}" "$dir"
+#done
+#
